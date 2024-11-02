753cca2fac0a7c984fe31411ee1018fb4b61e1f8
SM ORIGINALS <SMOS-Github@users.noreply.github.com>
2024-11-02 18:11:07 +0530

    Update
    Specially Designed For FireFox.

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("Alex");
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.inerimResults = true;
+
+let p = document.createElement('p');
+const voiceText = document.querySelector('.voice-text');
+voiceText.appendChild(p);
+
+recognition.addEventListener('result', e => {
+    const transcript = Array.from(e.result)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('');
+
+    p.textContent = transcript;
+    if (e.result[0].isfinal) {
+        p = document.createElement('p');
+        voiceText.appendChild(p);
+    }
+    console.log(transcript);
+});
+
+recognition.addEventListener('end', recognition.start);
+
+recognition.start();
